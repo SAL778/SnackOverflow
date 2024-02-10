8622409a1cdee8b501dc78b9989a4f9e786796b2
TauseefNafeeFattah <tauseefnafee@gmail.com>
2024-02-10 10:47:48 -0700

    added model

--- backend/api/models.py
+++ backend/api/models.py
@@ -42,3 +42,49 @@ class FollowRequest(models.Model):
 
     def __str__(self):
         return f'{self.from_user.display_name} sent a follow request to {self.to_user.display_name}'
+    
+#posts
+class Post(models.Model):
+    CONTENT_TYPES = (
+        ('text/markdown', 'text/markdown'),
+        ('text/plain', 'text/plain'),
+        ('application/base64', 'application/base64'),
+        ('image/png;base64', 'image/png;base64'),
+        ('image/jpeg;base64', 'image/jpeg;base64')
+    )
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    title = models.CharField(max_length=100)
+    source = models.URLField(max_length=200, blank=True)
+    origin = models.URLField(max_length=200, blank=True)
+    description = models.TextField()
+    contentType = models.CharField(max_length=50, choices=CONTENT_TYPES, default="text/markdown")
+    content = models.TextField()
+    author = models.ForeignKey(Author, related_name='posts_author', on_delete=models.CASCADE)
+    count = models.IntegerField(default=0)
+    comments = models.CharField(max_length=100, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    visibility = models.CharField(choices=['PUBLIC','FRIENDS','UNLISTED'])
+    image = models.URLField(max_length=200, blank=True, null=True)
+    #not sure if we need this
+    commentsSrc = models.JSONField(null=True, blank=True)
+
+#comments
+class Comment(models.Model):
+    id = models.UUIDField(primary_key=True,unique=True, default=uuid.uuid4, editable=False)
+    author = models.ForeignKey(User, related_name='author', on_delete=models.CASCADE)
+    comment=models.TextField()
+    contentType = models.CharField(max_length=50, default="text/markdown")
+    created_at = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+#likes
+class Like(models.Model):
+    author = models.ForeignKey(User, related_name='author', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="like_post", null=True)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name="like_comment", null=True)
+
+#inbox
+class Inbox(models.Model):
+    author = models.ForeignKey(Author, related_name='author', on_delete=models.CASCADE)
+    items = models.JSONField()
+    created_at = models.DateTimeField(auto_now_add=True)
