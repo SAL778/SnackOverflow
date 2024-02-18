29c81a28895426fa241ef6c3c9377dc586ae7a62
larizhang <zhang.tanya1@gmail.com>
2024-02-18 15:27:04 -0700

    added some css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import "./App.css";
 import Navigation from './components/Navbar.jsx'
+import NotificationBar from "./components/Notifbar.jsx";
 
 function App() {
 
 	return (
-	<div className="flex h-screen w-screen flex-col md:flex-row md:overflow-hidden">
+	<div className="flex h-screen w-screen flex-col md:flex-row md:overflow-hidden bg-gray-200">
     	<div className="flex-none md:w-60">
           <Navigation />
     	</div>
-		<div class="flex flex-1 bg-gray-300 h-16 p-4">Header</div>
+		<div class="flex flex-1 bg-gray-300 mx-4 my-3 h-16 p-4">Header</div>
+		<NotificationBar />
     </div>
 	);
 }
---
+++ frontend/src/components/Notifbar.jsx
@@ -0,0 +1,17 @@
+function Notification () {
+// should be scrollable? or just fit enough new notifications
+// check serializer
+    return (
+        <div>
+            {/* need to return all notification objects here */}
+        </div>
+    );
+}
+
+export default function NotificationBar() {
+    return (
+      <div className="flex flex-initial flex-col bg-white rounded-md shadow-md mx-4 my-3 h-4/6 w-3/12 p-4 overflow-hidden">
+        <h1 classname="">Notifications</h1>
+      </div>
+    );
+  }
