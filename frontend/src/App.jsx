813a9e107bc810e54ebf31c1f60b4d179c896a90
larizhang <zhang.tanya1@gmail.com>
2024-02-18 14:52:41 -0700

    removed base css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,11 @@ import Navigation from './components/Navbar.jsx'
 function App() {
 
 	return (
-	<div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-    	<div className="w-full flex-none md:w-64">
+	<div className="flex h-screen w-screen flex-col md:flex-row md:overflow-hidden">
+    	<div className="flex-none md:w-60">
           <Navigation />
     	</div>
+		<div class="flex flex-1 bg-gray-300 h-16 p-4">Header</div>
     </div>
 	);
 }
