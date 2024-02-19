fd97c200f9cc0a4cdd7f1b37e4d969acc38eee34
Soodarshan-Gajadhur <70911951+Soodarshan-Gajadhur@users.noreply.github.com>
2024-02-19 10:25:33 -0700

    integrate frontend and set up authentication

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', include('api.urls')),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('home/', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('features/', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
