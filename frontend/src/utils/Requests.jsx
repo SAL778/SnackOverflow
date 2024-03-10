fc7e2829ba7b29b29530a0461368440360927cd1
Soodarshan-Gajadhur <70911951+Soodarshan-Gajadhur@users.noreply.github.com>
2024-03-09 19:01:24 -0700

    api url

--- frontend/src/utils/Requests.jsx
+++ frontend/src/utils/Requests.jsx
@@ -6,7 +6,7 @@ axios.defaults.xsrfHeaderName = "X-CSRFToken";
 axios.defaults.withCredentials = true;
 axios.defaults.withXSRFToken = true;
 
-let baseURL = "https://snackoverflow-deployment-test-37cd2b94a62f.herokuapp.com/api/";
+let baseURL = process.env.REACT_APP_API_URL || null;
 
 if (process.env.NODE_ENV === "development") {
 	baseURL = "http://127.0.0.1:8000/api/";
