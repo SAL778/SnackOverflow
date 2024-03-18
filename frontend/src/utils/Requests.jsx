b1172354607b637f760aff63c3619b1ec04c0a77
TauseefNafeeFattah <tauseefnafee@gmail.com>
2024-03-18 15:27:39 -0600

    request base url

--- frontend/src/utils/Requests.jsx
+++ frontend/src/utils/Requests.jsx
@@ -6,9 +6,7 @@ axios.defaults.xsrfHeaderName = "X-CSRFToken";
 axios.defaults.withCredentials = true;
 axios.defaults.withXSRFToken = true;
 
-let baseURL = "http://127.0.0.1:8000/api/";
-// let baseURL =
-// 	"https://snackoverflow-deployment-test-37cd2b94a62f.herokuapp.com/api/";
+let baseURL = import.meta.env.VITE_API_URL || "https://dummyapi.com/api/";
 
 // if (process.env.NODE_ENV === "development") {
 // 	baseURL = "http://127.0.0.1:8000/api/";
