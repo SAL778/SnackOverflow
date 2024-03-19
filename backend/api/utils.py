b8e4121187a9889ec64f44d906c3c6d1c6c43861
TauseefNafeeFattah <tauseefnafee@gmail.com>
2024-03-19 09:28:42 -0600

    added image markdown remote

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -1,5 +1,5 @@
 import requests
-
+from .models import Node
 
 def get_remote_request(url, host):
     
@@ -18,3 +18,31 @@ def get_remote_request(url, host):
         return response.json()
     else:
         return None
+    
+def change_image_url(content, check_url ,api_url):
+    if f"![image]({check_url}" in content:
+        content = content.replace(f"![image]({check_url}", f"![image]({api_url}")
+    return content
+
+def check_content_type_and_change_url(content, nodes, api_url):
+    print(content)
+    if content.get('contentType') == 'text/markdown':
+        for node in nodes:
+            content['content'] = change_image_url(content.get('content'), node.api_url, api_url)
+    return content
+
+def get_our_host(request):
+    protocol = 'https' if request.is_secure() else 'http'
+    domain = request.get_host()
+    host_url = f"{protocol}://{domain}/api/"
+
+    return host_url
+
+def check_content(post, request):
+    nodes = Node.objects.all()
+
+    api_url = get_our_host(request)
+
+    post = check_content_type_and_change_url(post, nodes, api_url)
+
+    return post
